5e18c95f35985acfb5105d65273ff53dd9ad0ce5
teppei machida <machidanohimitsu@gmail.com>
2012-09-15 15:01:19 +0900

    add design template

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,17 @@
  */
 
 exports.index = function(req, res){
-  res.render('layout', { title: 'Rails Hackathon 2012 in Okinawa' })
+  res.renderPjax('about', { title: 'Rails Hackathon 2012 in Okinawa' })
+};
+
+exports.about_en = function(req, res){
+  res.renderPjax('about_en', { title: 'Okinawa RailsHackathon2012' })
+};
+
+exports.information = function(req, res){
+  res.renderPjax('information', { title: 'RailsHackathonについて' })
+};
+
+exports.schedule = function(req, res){
+  res.renderPjax('schedule', { title: 'RailsHackathonスケジュール' })
 };
--- web.js
+++ web.js
@@ -4,9 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
-var fs = require('fs');
-var less = require('less');
+  , pjax = require('express-pjax')
+  , routes = require('./routes')
 
 var app = module.exports = express.createServer(express.logger(),express.bodyParser());
 
@@ -15,61 +14,28 @@ var app = module.exports = express.createServer(express.logger(),express.bodyPar
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
+  app.use(pjax());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
-app.get("*.less", function(req, res) {
-    var path = __dirname + req.url;
-    fs.readFile(path, "utf8", function(err, data) {
-        if (err) throw err;
-        less.render(data, function(err, css) {
-            if (err) throw err;
-            res.header("Content-type", "text/css");
-            res.send(css);
-        });
-    });
-});
-
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+  app.use(express.errorHandler()); 
 });
 
 // Routes
 
-app.get('/', function(req, res){
-    res.render('about', {
-        locals: {title: 'Okinawa RailsHackathon2012'}
-    });
-});
+app.get('/', routes.index);
+app.get('/about_en', routes.about_en);
+app.get('/information', routes.information);
+app.get('/schedule', routes.schedule);
 
-app.get('/about_en', function(req, res){
-    res.render('about_en', {
-        locals: {title: 'Okinawa RailsHackathon2012'}
-    });
-});
-
-app.get('/information', function(req, res){
-    res.render('information', {
-        locals: {title: 'RailsHackathonについて'}
-    });
-});
-
-app.get('/schedule', function(req, res){
-    res.render('schedule', {
-        locals: {title: 'RailsHackathonスケジュール'}
-    });
-});
-
-var port = process.env.PORT || 3000;
-
-app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
